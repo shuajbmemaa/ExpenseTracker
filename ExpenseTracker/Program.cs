e0e6f3127d1e240a7af0ac87f02e02142e6c4e19
shuajbkingu <shuajbmemaa@hotmail.com>
2025-02-08 23:38:06 +0100

    Configure DbContext and AutoMapper services

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,3 +1,7 @@
+using ExpenseTracker.Data;
+using ExpenseTracker.Mapping;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(typeof(ApplicationMappingProfiles));
 
 var app = builder.Build();
 
