3433e0ed4ba1c0900437de1e28a11f2e79b1a2df
shuajbkingu <shuajbmemaa@hotmail.com>
2025-02-08 23:30:46 +0100

    Add initial models with relationships

---
+++ ExpenseTracker/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTracker.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Budget { get; set; }
+    public ICollection<Expense> Expenses { get; set; } = new List<Expense>();
+}
---
+++ ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseTracker.Models;
+
+public class Expense
+{
+    public int Id { get; set; }
+    public double Amount { get; set; }
+    public DateTime Date { get; set; }
+    public string? Description { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+}
+
