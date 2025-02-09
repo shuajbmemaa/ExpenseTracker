1fe46a61ac7d0934e010e4a56b93c547f87c6207
shuajbkingu <shuajbmemaa@hotmail.com>
2025-02-09 18:22:30 +0100

    Added Fluent Validation library to enforce validation rules

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,12 +1,17 @@
 using ExpenseTracker.Data;
 using ExpenseTracker.Mapping;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddFluentValidation(options =>
+    {
+        options.RegisterValidatorsFromAssemblyContaining<Program>();
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
