2112be37804ad1c949e0cd3d7fa6d92e9b16cd36
shuajbkingu <shuajbmemaa@hotmail.com>
2025-02-10 12:17:25 +0100

    Added UpdateDTO

---
+++ ExpenseTracker/Validators/UpdateExpenseDtoValidator.cs
@@ -0,0 +1,34 @@
+﻿using ExpenseTracker.DTOs;
+using FluentValidation;
+
+namespace ExpenseTracker.Validators;
+
+public class UpdateExpenseDtoValidator : AbstractValidator<UpdateExpenseDto>
+{
+    public UpdateExpenseDtoValidator()
+    {
+        When(x => x.Description != null, () =>
+        {
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description, if provided, cannot be empty.");
+        });
+
+        When(x => x.Amount.HasValue, () =>
+        {
+            RuleFor(x => x.Amount.Value)
+                .GreaterThan(0).WithMessage("Amount must be greater than 0.");
+        });
+
+        When(x => x.Date != null, () =>
+        {
+            RuleFor(x => x.Date)
+                .NotEmpty().WithMessage("Date, if provided, is required.");
+        });
+
+        When(x => x.CategoryId.HasValue, () =>
+        {
+            RuleFor(x => x.CategoryId.Value)
+                .GreaterThan(0).WithMessage("A valid category must be selected.");
+        });
+    }
+}
