c718422d47dda2cd86288dc09188a95f158f75fc
shuajbkingu <shuajbmemaa@hotmail.com>
2025-02-10 12:15:54 +0100

    Refactored the code using Repositories and Services

---
+++ ExpenseTracker/Repositories/IExpenseRepository.cs
@@ -0,0 +1,16 @@
+﻿using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Repositories;
+
+public interface IExpenseRepository
+{
+    Task<List<Expense>> GetAllExpensesAsync();
+    Task<Expense?> GetExpenseByIdAsync(int id);
+    Task<Expense> CreateExpenseAsync(Expense expense);
+    Task UpdateExpenseAsync(Expense expense);
+    Task DeleteExpenseAsync(int id);
+    Task<List<Expense>> GetExpensesByDateRangeAsync(DateTime fromDate, DateTime toDate);
+    Task<Expense?> GetMostExpensiveExpenseAsync();
+    Task<Expense?> GetLeastExpensiveExpenseAsync();
+    Task<int> GetTotalExpensesCountAsync();
+}
