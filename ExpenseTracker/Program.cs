6889f8cdb2817c33ddb96df33da4c9e472e077c2
shuajbkingu <shuajbmemaa@hotmail.com>
2025-02-10 12:19:46 +0100

    Register repository and service dependencies in DI container

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,5 +1,7 @@
 using ExpenseTracker.Data;
 using ExpenseTracker.Mapping;
+using ExpenseTracker.Repositories;
+using ExpenseTracker.Services;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +17,9 @@ builder.Services.AddControllers()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
+builder.Services.AddScoped<IExpenseService, ExpenseService>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
